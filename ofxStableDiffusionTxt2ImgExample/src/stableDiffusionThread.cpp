2721c47af760c8607f113245a53ff74959ef065a
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-12-01 05:12:13 +0100

    Delete ofxStableDiffusionTxt2ImgExample directory

--- ofxStableDiffusionTxt2ImgExample/src/stableDiffusionThread.cpp
+++
@@ -1,6 +0,0 @@
-#include "stableDiffusionThread.h"
-
-void stableDiffusionThread::threadedFunction() {
-	stableDiffusionPixelVector = stableDiffusion.txt2img(prompt, negativePrompt, cfgScale, width, height, sampleMethod, sampleSteps, seed, batch_count);
-	diffused = true;
-}
