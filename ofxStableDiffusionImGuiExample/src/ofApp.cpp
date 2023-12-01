0ffc32bb3c82c8a079e5a1f803d5c68fe1a45a1c
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-12-01 06:08:35 +0100

    Update ofApp.cpp

--- ofxStableDiffusionImGuiExample/src/ofApp.cpp
+++ ofxStableDiffusionImGuiExample/src/ofApp.cpp
@@ -57,7 +57,7 @@ void ofApp::update() {
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-	ImGuiWindowFlags flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoScrollbar;
+	ImGuiWindowFlags flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoSavedSettings;
 	static bool log_open{ true };
 	struct Funcs
 	{
