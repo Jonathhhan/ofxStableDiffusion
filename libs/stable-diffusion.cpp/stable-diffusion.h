e31dc5c873a92abf79fab83e85020db793651597
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-12-05 16:14:30 +0100

    Add files via upload

--- libs/stable-diffusion.cpp/stable-diffusion.h
+++ libs/stable-diffusion.cpp/stable-diffusion.h
@@ -1,16 +1,35 @@
 #ifndef __STABLE_DIFFUSION_H__
 #define __STABLE_DIFFUSION_H__
 
-#include <string>
 #include <memory>
+#include <string>
 #include <vector>
 
-enum SDLogLevel {
-    DEBUG,
-    INFO,
-    WARN,
-    SD_ERROR
+enum ggml_type {
+    GGML_TYPE_F32 = 0,
+    GGML_TYPE_F16 = 1,
+    GGML_TYPE_Q4_0 = 2,
+    GGML_TYPE_Q4_1 = 3,
+    // GGML_TYPE_Q4_2 = 4, support has been removed
+    // GGML_TYPE_Q4_3 (5) support has been removed
+    GGML_TYPE_Q5_0 = 6,
+    GGML_TYPE_Q5_1 = 7,
+    GGML_TYPE_Q8_0 = 8,
+    GGML_TYPE_Q8_1 = 9,
+    // k-quantizations
+    GGML_TYPE_Q2_K = 10,
+    GGML_TYPE_Q3_K = 11,
+    GGML_TYPE_Q4_K = 12,
+    GGML_TYPE_Q5_K = 13,
+    GGML_TYPE_Q6_K = 14,
+    GGML_TYPE_Q8_K = 15,
+    GGML_TYPE_I8,
+    GGML_TYPE_I16,
+    GGML_TYPE_I32,
+    GGML_TYPE_COUNT,
 };
+
+
 
 enum RNGType {
     STD_DEFAULT_RNG,
@@ -45,11 +64,14 @@ private:
 public:
     void setup(int n_threads                = -1,
                     bool vae_decode_only         = false,
-                    std::string taesd_path       = "",
+                    std::string taesd_path       = "data/models/taesd/diffusion_pytorch_model.SAFETENSORS",
                     bool free_params_immediately = false,
-                    std::string lora_model_dir   = "",
+                    std::string lora_model_dir   = "data/models/loras/",
                     RNGType rng_type             = STD_DEFAULT_RNG);
-    bool load_from_file(const std::string& file_path, Schedule d = DEFAULT);
+    bool load_from_file(const std::string& model_path,
+                        const std::string& vae_path,
+                        ggml_type wtype,
+                        Schedule d = DEFAULT);
     std::vector<uint8_t*> txt2img(
         std::string prompt,
         std::string negative_prompt,
@@ -74,7 +96,6 @@ public:
         int64_t seed);
 };
 
-void set_sd_log_level(SDLogLevel level);
 std::string sd_get_system_info();
 
 #endif  // __STABLE_DIFFUSION_H__
