9a8e159c7ef026c3683b48f8045b506413bd16af
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-12-05 03:11:49 +0100

    Update main.cpp

--- ofxStableDiffusionImGuiExample/src/main.cpp
+++ ofxStableDiffusionImGuiExample/src/main.cpp
@@ -6,10 +6,8 @@ int main( ){
 
 	ofGLFWWindowSettings settings;
 	settings.setSize(1280, 720);
-	// settings.setPosition(glm::vec2(0,0));
 	// settings.maximized = true;
 	settings.visible = true;
-	settings.monitor = 0;
 	settings.multiMonitorFullScreen = false;
 	settings.windowMode = OF_WINDOW;
 
